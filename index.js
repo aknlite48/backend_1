1142aac2101a36d2529655f35745d1b46ad251a8
aknlite48 <aanandk2705@gmail.com>
2024-08-13 05:03:30 -0400

    added get and delete

--- index.js
+++ index.js
@@ -27,6 +27,25 @@ app.get('/api/notes',(request,response)=>{
   response.json(notes)
 })
 
+app.get('/api/notes/:id',(request,response)=>{
+  const id = request.params.id
+  console.log(response)
+  const note = notes.find((n)=>{return n.id===id})
+  if (note) {
+    response.json(note)
+  }
+  else {
+    response.status(404).end()
+  }
+  
+})
+
+app.delete('/api/notes/:id',(request,response)=>{
+  const id = request.params.id
+  notes = notes.filter((n)=>{return n.id!==id})
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT,()=>{})
 console.log(`server running on ${PORT}`)
